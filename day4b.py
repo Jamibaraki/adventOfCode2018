0ac54f0f319546cde6a721195aebbb697404987a
James <jameskonik@gmail.com>
2018-12-04 17:52:55 +0900

    day 4

---
+++ day4b.py
@@ -0,0 +1,93 @@
+file = open('input4.txt','r')
+#file = open('inputTemp.txt','r')
+
+input = file.read().split("\n")
+file.close()
+
+input.sort()
+
+
+guards = []
+currentGuard = 0
+
+#extract minutes from timestamp
+def getMinutes( value ):
+    return int( value.split( ':')[1][:2] )
+
+def doesGuardExist( id ):
+    for guard in guards:
+        if guard[0] == id:
+            return True
+    return False
+
+def getGuardIndex( id ):
+    for x in range(0, len(guards)):
+        if guards[x][0] == id:
+            return x
+    print("Error: getGuardIndex failed")
+    return -1
+
+
+#build a list of guards with their sleep periods
+candidateRange = [] #temporarily store a range of times
+
+for item in input:
+    parts = item.split( ' ')
+    if parts[2] == 'Guard':
+        currentGuard = int (parts[3][1:])
+    if parts[2] == 'falls':
+        candidateRange = []
+        candidateRange.append( getMinutes(parts[1]))
+    if parts[2] == 'wakes':
+        candidateRange.append(getMinutes(parts[1]))
+        if doesGuardExist(currentGuard) == False:
+            newGuard = []
+            newGuard.append(currentGuard)
+            newGuard.append(candidateRange)
+            guards.append(newGuard)
+        else:
+            index = getGuardIndex( currentGuard )
+            guards[index].append(candidateRange)
+
+mostSleepGuard = -1
+maxSleepyTime = 0
+
+maxMinsAsleep = []
+
+recordGuard = 0
+recordSleepMinutes = 0
+recordResult = 0
+
+for guard in guards:
+    minutes = []
+    for x in range(0,60):
+        minutes.append( 0 )
+    for x in range(1, len(guard)):
+        for y in range(guard[x][0], guard[x][1]):
+            minutes[y] = minutes[y] + 1
+    biggestMin = 0
+    mostSleep = 0
+    for x in range(0, len(minutes)):
+        if (minutes[x] > mostSleep):
+            mostSleep = minutes[x]
+            biggestMin = x
+
+    currentInfo = []
+    currentInfo.append( guard[0 ]) #append his id
+    currentInfo.append( biggestMin )
+    currentInfo.append( mostSleep )
+    if mostSleep > recordSleepMinutes:
+        recordGuard = guard[0]
+        recordSleepMinutes = mostSleep
+        recordResult = biggestMin * guard[0]
+
+
+print( recordResult )
+
+
+
+
+
+
+
+
